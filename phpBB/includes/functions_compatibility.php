746ecf9e48f82d489a623e1a27d3dcfa5a6a049a
Marc Alexander <admin@m-a-styles.de>
2018-12-28 10:59:02 +0100

    [ticket/10824] Move parse_cfg_file() to functions_compatibility
    PHPBB3-10824

--- phpBB/includes/functions_compatibility.php
+++ phpBB/includes/functions_compatibility.php
@@ -640,3 +640,65 @@ function phpbb_email_hash($email)
 {
 	return sprintf('%u', crc32(strtolower($email))) . strlen($email);
 }
+
+/**
+ * Parse cfg file
+ * @param string $filename
+ * @param bool|array $lines
+ * @return array
+ *
+ * @deprecated 3.3.0-a1 (To be removed: 4.0.0)
+ */
+function parse_cfg_file($filename, $lines = false)
+{
+	$parsed_items = array();
+
+	if ($lines === false)
+	{
+		$lines = file($filename);
+	}
+
+	foreach ($lines as $line)
+	{
+		$line = trim($line);
+
+		if (!$line || $line[0] == '#' || ($delim_pos = strpos($line, '=')) === false)
+		{
+			continue;
+		}
+
+		// Determine first occurrence, since in values the equal sign is allowed
+		$key = htmlspecialchars(strtolower(trim(substr($line, 0, $delim_pos))));
+		$value = trim(substr($line, $delim_pos + 1));
+
+		if (in_array($value, array('off', 'false', '0')))
+		{
+			$value = false;
+		}
+		else if (in_array($value, array('on', 'true', '1')))
+		{
+			$value = true;
+		}
+		else if (!trim($value))
+		{
+			$value = '';
+		}
+		else if (($value[0] == "'" && $value[strlen($value) - 1] == "'") || ($value[0] == '"' && $value[strlen($value) - 1] == '"'))
+		{
+			$value = htmlspecialchars(substr($value, 1, strlen($value)-2));
+		}
+		else
+		{
+			$value = htmlspecialchars($value);
+		}
+
+		$parsed_items[$key] = $value;
+	}
+
+	if (isset($parsed_items['parent']) && isset($parsed_items['name']) && $parsed_items['parent'] == $parsed_items['name'])
+	{
+		unset($parsed_items['parent']);
+	}
+
+	return $parsed_items;
+}
